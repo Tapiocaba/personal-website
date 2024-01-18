0fdc0aa639894373229ef31e6668b9befec33c97
Lisa Lin <lisalin@college.harvard.edu>
2024-01-17 19:21:38 -0600

    add resources + made archives better

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 1-day personal website built with React to procrastinate :) *If you're my manager and watching me update my personal website instead of responding to pr #932 on linear please let me cook <3*
 
-Inspired by [quinn ha](https://www.quinnha.xyz/), [chengsu chen](https://www.chengsuchen.com/), and everyone's websites that I stalked :)
+Inspired by [quinn ha](https://www.quinnha.xyz/), [chengsu chen](https://www.chengsuchen.com/), [catherine yeo](https://catherinehyeo.com/), and everyone's websites that I stalked :)
 
 ## To-do:
 
--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 
 import { PageTitle, Text, SmallBreak, LargeBreak } from '../components/PageStyles';
 
+import { RedLink } from '../components/PageStyles';
 import archives from '../components/archives.json';
 
 export default function Archive() {
@@ -16,10 +17,18 @@ export default function Archive() {
             <LargeBreak />
 
             <Text as="h1" sx={{ fontWeight: 'bold', fontSize: '1.4em' }}>club club stuff</Text>
+            <Text> this is me realizing that all of harvard's clubs start with "harvard." </Text>
+
             <ul>
                 {archives.clubs.map((club, index) => (
                     <li key={index}>
-                        <Text>{club}</Text>
+                        {club.url ? (
+                            <Text>
+                                {club.description} (<RedLink href={club.url}>{club.linkText}</RedLink>)
+                            </Text>
+                        ) : (
+                            <Text>{club}</Text>
+                        )}
                     </li>
                 ))}
             </ul>
@@ -27,13 +36,28 @@ export default function Archive() {
 
             <Text as="h1" sx={{ fontWeight: 'bold', fontSize: '1.5em' }}>courses</Text>
             <Text as="p">* = graduate courses</Text>
+            <Text as="p">notes courtesy of goodnotes, figma, and joe blitzstein</Text>
+            <Text>longer vers. of 124 notes coming after i fix my ipad</Text>
+            <SmallBreak />
+
             {archives.courses.map((term, index) => (
                 <React.Fragment key={index}>
                     <Text as="h3" sx={{ fontWeight: 'bold' }}>{term.term}</Text>
                     <ul>
                         {term.classes.map((course, courseIndex) => (
                             <li key={courseIndex}>
-                                <Text>{course}</Text>
+                                <Text>
+                                    {course.name}
+                                    {course.resource && (
+                                        <>
+                                            {' ['}
+                                            <RedLink href={course.resource} target="_blank">
+                                                Notes
+                                            </RedLink>
+                                            {']'}
+                                        </>
+                                    )}
+                                </Text>
                             </li>
                         ))}
                     </ul>
